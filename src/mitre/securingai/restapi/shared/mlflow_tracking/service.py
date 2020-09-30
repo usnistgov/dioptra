f7f4e4af77b1f58bfbbb31ebbc6aa748b4a217e2
James K. Glasbrenner <jglasbrenner@mitre.org>
2020-09-29 22:38:59 -0400

    feat(restapi): implement shared mlflow tracking client service
    Create client class for the API to connect to the MLFlow Tracking service and perform CRUD
    operations.

---
+++ src/mitre/securingai/restapi/shared/mlflow_tracking/service.py
@@ -0,0 +1,72 @@
+from typing import Optional
+
+import structlog
+from injector import inject
+from mlflow.exceptions import RestException
+from mlflow.tracking import MlflowClient
+from structlog import BoundLogger
+from structlog._config import BoundLoggerLazyProxy
+
+
+LOGGER: BoundLoggerLazyProxy = structlog.get_logger()
+
+
+class MLFlowTrackingService(object):
+    @inject
+    def __init__(self, client: MlflowClient) -> None:
+        self._client = client
+
+    def create_experiment(self, experiment_name: str, **kwargs) -> Optional[str]:
+        log: BoundLogger = kwargs.get("log", LOGGER.new())
+
+        try:
+            return self._client.create_experiment(name=experiment_name)
+
+        except RestException as e:
+            if e.error_code == "RESOURCE_ALREADY_EXISTS":
+                log.warning(
+                    "MLFlow experiment already exists", experiment_name=experiment_name
+                )
+                return None
+
+            raise e
+
+    def delete_experiment(self, experiment_id: int, **kwargs) -> Optional[bool]:
+        log: BoundLogger = kwargs.get("log", LOGGER.new())
+
+        try:
+            self._client.delete_experiment(experiment_id=experiment_id)
+
+        except RestException as e:
+            if e.error_code == "RESOURCE_DOES_NOT_EXIST":
+                log.warning(
+                    "MLFlow experiment does not exist", experiment_id=experiment_id
+                )
+                return None
+
+            raise e
+
+        return True
+
+    def rename_experiment(
+        self, experiment_id: int, new_name: str, **kwargs
+    ) -> Optional[bool]:
+        log: BoundLogger = kwargs.get("log", LOGGER.new())
+
+        try:
+            self._client.rename_experiment(
+                experiment_id=experiment_id, new_name=new_name
+            )
+
+        except RestException as e:
+            if e.error_code == "RESOURCE_DOES_NOT_EXIST":
+                log.warning(
+                    "MLFlow experiment does not exist",
+                    experiment_id=experiment_id,
+                    new_name=new_name,
+                )
+                return None
+
+            raise e
+
+        return True
