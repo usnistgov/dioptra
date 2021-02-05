0e78da8a4fb05b2a47eaa5724d57b152957fe8e4
Andrew Hand <ahand@mitre.org>
2021-02-05 12:34:00 -0500

    docs: add sphinx_copybutton dependency to configuration file

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -34,6 +34,7 @@ extensions = [
     "sphinx.ext.napoleon",
     "sphinx.ext.viewcode",
     "recommonmark",
+    "sphinx_copybutton",
 ]
 
 # Add any paths that contain templates here, relative to this directory.
