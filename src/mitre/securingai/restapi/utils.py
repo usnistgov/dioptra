f48e6b9321a5c0df9b7767e6b48e101d7010d001
James K. Glasbrenner <jglasbrenner@mitre.org>
2021-03-29 10:37:16 -0400

    feat(restapi): add utility to help convert schemas into swagger docs

---
+++ src/mitre/securingai/restapi/utils.py
@@ -0,0 +1,47 @@
+"""A module of utility functions that are used across multiple endpoints.
+
+.. |Namespace| replace:: :py:class:`~flask_restx.Namespace`
+.. |ParametersSchema| replace:: :py:class:`~.ParametersSchema`
+.. |RequestParser| replace:: :py:class:`~flask_restx.reqparse.RequestParser`
+.. |Resource| replace:: :py:class:`~flask_restx.Resource`
+"""
+
+from typing import List
+
+from flask_restx import Namespace
+from flask_restx.reqparse import RequestParser
+from typing_extensions import TypedDict
+
+
+class ParametersSchema(TypedDict, total=False):
+    """A schema of the parameters that can be passed to the |RequestParser|."""
+
+    name: str
+    type: object
+    location: str
+    help: str
+
+
+def as_api_parser(
+    api: Namespace, parameters_schema: List[ParametersSchema]
+) -> RequestParser:
+    """Creates a |RequestParser| from a list of |ParametersSchema| dictionaries.
+
+    The primary use case for this utility function is to convert a submission form
+    schema so that it can be passed to an `@api.expects` decorator and populate the
+    form's fields in the Swagger documentation.
+
+    Args:
+        api: An endpoint's |Namespace| used for grouping |Resource|s.
+        parameters_schema: A list of |ParametersSchema| dictionaries to add as arguments
+            to `api.parser()`.
+
+    Returns:
+        A |RequestParser| object.
+    """
+    parser: RequestParser = api.parser()
+
+    for form_kwargs in parameters_schema:
+        parser.add_argument(**form_kwargs)
+
+    return parser
