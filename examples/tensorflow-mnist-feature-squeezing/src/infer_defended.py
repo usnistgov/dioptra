57aa1be3231a601ffdf53018fb1ab5575283f7b6
TrellixVulnTeam <charles.mcfarland@trellix.com>
2022-10-20 02:03:03 +0000

    Adding tarfile member sanitization to extractall()

--- examples/tensorflow-mnist-feature-squeezing/src/infer_defended.py
+++ examples/tensorflow-mnist-feature-squeezing/src/infer_defended.py
@@ -138,7 +138,29 @@ def infer_adversarial(run_id, model, model_architecture, batch_size, seed, bit_d
         )
 
         with tarfile.open(adv_testing_tar_path, "r:gz") as f:
-            f.extractall(path=Path.cwd())
+            
+            import os
+            
+            def is_within_directory(directory, target):
+                
+                abs_directory = os.path.abspath(directory)
+                abs_target = os.path.abspath(target)
+            
+                prefix = os.path.commonprefix([abs_directory, abs_target])
+                
+                return prefix == abs_directory
+            
+            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            
+                for member in tar.getmembers():
+                    member_path = os.path.join(path, member.name)
+                    if not is_within_directory(path, member_path):
+                        raise Exception("Attempted Path Traversal in Tar File")
+            
+                tar.extractall(path, members, numeric_owner=numeric_owner) 
+                
+            
+            safe_extract(f, path=Path.cwd())
 
         adv_ds = create_image_dataset(
             data_dir=str(adv_testing_data_dir.resolve()),
