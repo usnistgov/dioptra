2e30719154589b4d47eaebbe0878e6b2aeb74986
Julian T Sexton <JTSEXTON@MITRE.ORG>
2023-03-02 13:04:07 -0500

    test: add unit tests for utils.py

--- tests/unit/task_plugins/dioptra_builtins/artifacts/test_utils.py
+++ tests/unit/task_plugins/dioptra_builtins/artifacts/test_utils.py
@@ -26,27 +26,111 @@ from typing import Union
 import pytest
 import structlog 
 import tarfile 
+import os
+import shutil
+
+
 
 @pytest.mark.parametrize(
     "filepath",
     [
+        "tmp_unit_test.tar.gz"
+    ],
+)
+@pytest.mark.parametrize(
+    ("dirs", "toremove", "filenames"),
+    [
+        ([
+            "tmp_unit_test/test/dir/for/unit/tests", 
+            "tmp_unit_test/test/dir/for/unit/things",
+            "tmp_unit_test/files/dir"
+        ], "tmp_unit_test", ["test_random.py", "test_utils.py", "test_xyz.py"]),
     ],
 )
 @pytest.mark.parametrize(
     "tarball_read_mode",
     [
+        "r:gz"
     ],
 )
-def test_extract_tarfile(filepath, tarball_read_mode) -> None:
+def test_extract_tarfile(filepath,  dirs, toremove, filenames, tarball_read_mode) -> None:
     from dioptra_builtins.artifacts.utils import extract_tarfile
-    pass
+    # generate files for tarfile
+    filelist = []
+    for m in dirs: 
+        for q in filenames:
+            path_to_file = Path(m) / q
+            filelist += [path_to_file]
+            if not os.path.exists(m):
+                os.makedirs(Path(m))
+            f = open(path_to_file, "w")
+            f.write("random text")
+            f.close()
+
+    for p in filelist:
+        assert os.path.exists(p) 
+
+    # create temporary tarfile
+    tar_file = tarfile.open(filepath,"w:gz")
+    for z in filelist:
+        tar_file.add(z)
+    tar_file.close()
+    
+    # delete directories
+
+    for dd in dirs:
+        for root, ds, files in os.walk(dd):
+            for f in files:
+                os.unlink(os.path.join(root, f))
+            for d in ds:
+                shutil.rmtree(os.path.join(root, d))
+            
+    #check successful deletion
+    for p in filelist:
+        print(p)
+        assert not os.path.exists(p) 
+
+    #check tarfile exists
+    assert os.path.exists(filepath)
+    
+    # extract tarfile
+    extract_tarfile(filepath, tarball_read_mode) 
+    
+    # check tarfile extraction
+    for p in filelist:
+        assert os.path.exists(p)
+    
+    # clean up tarfile
+    os.remove(filepath)
+    
+    # clean up directories
+    for z in toremove: 
+        if os.path.exists(z):
+            shutil.rmtree(z)
+    
+    
 
 @pytest.mark.parametrize(
-    "dirs",
+    ("dirs", "toremove"),
     [
+        ([
+            "tmp_unit_test/test/dir/for/unit/tests", 
+            "tmp_unit_test/test/dir/for/unit/things",
+            "tmp_unit_test/files/dir"
+        ], ["tmp_unit_test"]),
+        ([],[]),
+        (["tmp_unit_test_srcs"], ["tmp_unit_test_srcs"])
     ],
 )
-def test_make_directories(dirs) -> None:
+def test_make_directories(dirs, toremove) -> None:
     from dioptra_builtins.artifacts.utils import make_directories
-    pass
+    for z in toremove:
+        if os.path.exists(z):
+            shutil.rmtree( z )
+    assert not any([os.path.exists(d) for d in dirs])
+    make_directories(dirs)
+    assert all([os.path.isdir(d) for d in dirs])
+    for z in toremove:
+        if os.path.exists(z):
+            shutil.rmtree( z )
 
