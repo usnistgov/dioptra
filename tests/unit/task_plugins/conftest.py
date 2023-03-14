7b3b5bf213e8b4f843f9061d7a1c9603ebafe4f0
jtsextonMITRE <45762017+jtsextonMITRE@users.noreply.github.com>
2023-03-14 11:01:32 -0400

    test: add unit tests for the task plugins
    Add unit tests to improve code coverage of the base code with tests for the task plugins.
    
    Closes #121

---
+++ tests/unit/task_plugins/conftest.py
@@ -0,0 +1,33 @@
+# This Software (Dioptra) is being made available as a public service by the
+# National Institute of Standards and Technology (NIST), an Agency of the United
+# States Department of Commerce. This software was developed in part by employees of
+# NIST and in part by NIST contractors. Copyright in portions of this software that
+# were developed by NIST contractors has been licensed or assigned to NIST. Pursuant
+# to Title 17 United States Code Section 105, works of NIST employees are not
+# subject to copyright protection in the United States. However, NIST may hold
+# international copyright in software created by its employees and domestic
+# copyright (or licensing rights) in portions of software that were assigned or
+# licensed to NIST. To the extent that NIST holds copyright in this software, it is
+# being made available under the Creative Commons Attribution 4.0 International
+# license (CC BY 4.0). The disclaimers of the CC BY 4.0 license apply to all parts
+# of the software developed or licensed by NIST.
+#
+# ACCESS THE FULL CC BY 4.0 LICENSE HERE:
+# https://creativecommons.org/licenses/by/4.0/legalcode
+from __future__ import annotations
+
+from pathlib import Path
+
+import pytest
+
+from dioptra.sdk.utilities.contexts import plugin_dirs
+
+TASK_PLUGINS_DIR = (
+    Path(__file__).parent / ".." / ".." / ".." / "task-plugins"
+).resolve()
+
+
+@pytest.fixture(scope="session", autouse=True)
+def plugin_dirs_context():
+    with plugin_dirs([TASK_PLUGINS_DIR]):
+        yield
