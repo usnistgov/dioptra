bf258f687696e99996ef6e70ba51af1d1a5249d4
andrewhand <95315138+andrewhand@users.noreply.github.com>
2023-04-07 09:55:58 -0400

    Added custom templates

---
+++ src/frontend/src/templates/button.vue
@@ -0,0 +1,25 @@
+<template>
+  <button :class="btnClass" @click="handleClick">
+    {{ buttonText }}
+  </button>
+</template>
+
+<script>
+export default {
+  props: {
+    buttonText: {
+      type: String,
+      default: 'Button'
+    },
+    btnClass: {
+      type: String,
+      default: 'btn'
+    }
+  },
+  methods: {
+    handleClick() {
+      this.$emit('click');
+    }
+  }
+};
+</script>
---
+++ src/frontend/src/templates/searchbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <form class="form-inline my-2 my-lg-0" @submit.prevent="handleSearch">
+    <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search" v-model="searchTerm">
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+  </form>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      searchTerm: ''
+    };
+  },
+  methods: {
+    handleSearch() {
+      this.$emit('search', this.searchTerm);
+    }
+  }
+};
+</script>
