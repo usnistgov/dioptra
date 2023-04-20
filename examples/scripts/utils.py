6e05d46bc848acbe9683332e4687876ced008509
Luke Barber <lbarber@mm287324-pc.myfiosgateway.com>
2023-04-20 11:20:00 -0400

    examples: refactor usage of the tensorflow-mnist-classifier example
    Refactors the tensorflow-mnist-classifier demo to use the new, common approach designed for the
    examples folder. This includes the following changes:
    
    - Moving the task plugins to the root of the examples folder
    - Implementing a script to register all custom task plugins for the examples as a preliminary step
    - Implementing a script to register all queues for the examples as a preliminary step
    - Extracted the RichConsole class to the utils.py file in the scripts folder so it could be shared
      and used across multiple CLI scripts.
    - Updating the documentation in the examples/README.md and
      examples/tensorflow-mnist-classifier/README.md files with instructions on how the user should
      use the above scripts to prepare their environment for running the examples.
    - Updated the list of dependencies in the examples/scripts/venvs/examples-setup-requirements.txt
      file to include additional dependencies that are needed to run the files in the scripts folder and
      to run the Jupyter notebooks.
    - Moved the MLproject file into the src folder in tensorflow-mnist-classifier
    - Modifying the demo Jupyter notebook for tensorflow-mnist-classifier to use the new scripts
    - Removing any files from the tensorflow-mnist-classifier folder that were no longer being used
      after implementing the above changes.
    
    Closes #177
    Co-authored-by: James K. Glasbrenner <jglasbrenner@mitre.org>

--- examples/scripts/utils.py
+++ examples/scripts/utils.py
@@ -14,11 +14,97 @@
 #
 # ACCESS THE FULL CC BY 4.0 LICENSE HERE:
 # https://creativecommons.org/licenses/by/4.0/legalcode
+"""A set of shared utility classes and functions.
+
+Classes:
+    RichConsole: A simple API for printing formatted text in the terminal.
+
+Functions:
+    make_tar: Archive a list of directories and files.
+"""
 from __future__ import annotations
 
 import os
 import tarfile
 from pathlib import Path
+
+from rich.console import Console
+from rich.panel import Panel
+
+
+class RichConsole(object):
+    """A simple API for printing formatted text in the terminal.
+
+    Args:
+        console: A rich.console.Console object.
+    """
+
+    def __init__(self, console: Console) -> None:
+        self._console = console
+
+    def print_alert(self, text: str) -> None:
+        """Print an alert message.
+
+        Args:
+            text: The text to print.
+        """
+        content: str = f":heavy_exclamation_mark: {text}"
+        self._console.print(content)
+
+    def print_failure(self, text: str) -> None:
+        """Print a failure message.
+
+        Args:
+            text: The text to print.
+        """
+        content: str = f":x:  {text}"
+        self._console.print(content)
+
+    def print_info(self, text: str) -> None:
+        """Print an informational message.
+
+        Args:
+            text: The text to print.
+        """
+        content: str = f"[bold yellow]Ⓘ[/bold yellow]  {text}"
+        self._console.print(content)
+
+    def print_parameter(self, name: str, value: str) -> None:
+        """Print a parameter name and its value.
+
+        Args:
+            name: The name of the parameter.
+            value: The value of the parameter.
+        """
+        content: str = f" ‣ [bold]{name}:[/bold] {value}"
+        self._console.print(content)
+
+    def print_success(self, text: str) -> None:
+        """Print a success message.
+
+        Args:
+            text: The text to print.
+        """
+        content: str = f" [bold bright_green]✔[/bold bright_green] {text}"
+        self._console.print(content)
+
+    def print_title(self, text: str) -> None:
+        """Print a title.
+
+        Args:
+            text: The text to print.
+        """
+        content: Panel = Panel(renderable=text, expand=False)
+        self._console.print(content, style="bold cyan")
+
+    def print_warning(self, text: str) -> None:
+        """Print a warning message.
+
+        Args:
+            text: The text to print.
+        """
+        content: str = f":warning: {text}"
+        self._console.print(content)
 
 
 def make_tar(
@@ -28,7 +114,7 @@ def make_tar(
     working_dir: str | Path | None = None,
 ) -> Path:
     """Archive a list of directories and files.
-    
+
     This implementation flattens the source directory structure so that all the files
     are placed in the root of the archive, and then the archive is compressed.
 
@@ -57,7 +143,7 @@ def make_tar(
         for dir in source_dir:
             dir = Path(dir)
             if dir.is_dir():
-                for dirpath, dirnames, filenames in os.walk(dir):
+                for dirpath, _, filenames in os.walk(dir):
                     for name in filenames:
                         name = Path(dirpath, name)
                         f.add(name, name.name)
