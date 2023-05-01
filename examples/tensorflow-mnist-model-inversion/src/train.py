f4eb4edadf59f6cec7cae4dc9f722eceaaa3215a
Luke Barber <lbarber@mm287324-pc.fios-router.home>
2023-05-01 18:08:10 -0400

    examples: refactor the tensorflow-mnist-model-inversion example
    Refactors the tensorflow-mnist-model-inversion demo to use the new, common approach designed for the
    examples folder. This includes the following changes:
    
    - Moved any new custom task plugins from tensorflow-mnist-model-inversion folder to the root of the
      examples folder.
    - Moved the MLproject file into the src folder.
    - Updated the src/train.py script to use the log_tensorflow_keras_estimator task plugin to log the
      trained model.
    - Updated the documentation in examples/tensorflow-mnist-model-inversion/README.md with instructions
      on how the user should prepare their environment for running the examples.
    - Modified the demo Jupyter notebook for tensorflow-mnist-model-inversion to use the new scripts.
    - Removed any files from the tensorflow-mnist-model-inversion folder that were no longer being used
      after implementing the above changes.

--- examples/tensorflow-mnist-model-inversion/src/train.py
+++ examples/tensorflow-mnist-model-inversion/src/train.py
@@ -153,8 +153,6 @@ def train(
         seed=seed,
     )
 
-    mlflow.autolog()
-
     with mlflow.start_run() as active_run:
         flow: Flow = init_train_flow()
         state = flow.run(
@@ -238,22 +236,21 @@ def init_train_flow() -> Flow:
             "get_optimizer",
             optimizer_name,
             learning_rate=learning_rate,
-            upstream_tasks=[init_tensorflow_results]
+            upstream_tasks=[init_tensorflow_results],
         )
-        
         metrics = pyplugs.call_task(
             f"{_CUSTOM_PLUGINS_IMPORT_PATH}.evaluation",
             "tensorflow",
             "get_performance_metrics",
             PERFORMANCE_METRICS,
-            upstream_tasks=[init_tensorflow_results]
+            upstream_tasks=[init_tensorflow_results],
         )
         callbacks_list = pyplugs.call_task(
             f"{_CUSTOM_PLUGINS_IMPORT_PATH}.evaluation",
             "tensorflow",
             "get_model_callbacks",
             CALLBACKS,
-            upstream_tasks=[init_tensorflow_results]
+            upstream_tasks=[init_tensorflow_results],
         )
         training_ds = pyplugs.call_task(
             f"{_PLUGINS_IMPORT_PATH}.data",
@@ -327,13 +324,21 @@ def init_train_flow() -> Flow:
             "evaluate_metrics_tensorflow",
             classifier=classifier,
             dataset=testing_ds,
-            upstream_tasks=[history]
+            upstream_tasks=[history],
         )
         log_classifier_performance_metrics_result = pyplugs.call_task(  # noqa: F841
             f"{_PLUGINS_IMPORT_PATH}.tracking",
             "mlflow",
             "log_metrics",
             metrics=classifier_performance_metrics,
+        )
+        logged_tensorflow_keras_estimator = pyplugs.call_task(
+            f"{_PLUGINS_IMPORT_PATH}.tracking",
+            "mlflow",
+            "log_tensorflow_keras_estimator",
+            estimator=classifier,
+            model_dir="model",
+            upstream_tasks=[history],
         )
         model_version = pyplugs.call_task(  # noqa: F841
             f"{_PLUGINS_IMPORT_PATH}.registry",
