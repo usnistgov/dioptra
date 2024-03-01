d8c2fb060440a3df9491e07f50c975bca41bd98a
etrapnell-nist <eric.trapnell@nist.gov>
2024-03-01 16:12:41 -0500

    feat: implement v1 schemas for MLFlow Runs

--- src/dioptra/restapi/routes.py
+++ src/dioptra/restapi/routes.py
@@ -35,6 +35,7 @@ USER_ROUTE = "user"
 V1_PLUGINS_ROUTE = "plugins"
 V1_PLUGIN_PARAMETER_TYPES_ROUTE = "pluginParameterTypes"
 V1_QUEUES_ROUTE = "queues"
+V1_MLFLOW_RUNS_ROUTE = "mlflow_runs"
 
 
 def register_routes(api: Api) -> None:
@@ -74,8 +75,10 @@ def register_v1_routes(api: Api) -> None:
     Args:
         api: The main REST |Api| object.
     """
+    from .v1.mlflow_runs.controller import api as mlflow_runs_api
     from .v1.plugins.controller import api as plugins_api
     from .v1.queues.controller import api as queues_api
 
     api.add_namespace(plugins_api, path=f"/{V1_ROOT}/{V1_PLUGINS_ROUTE}")
     api.add_namespace(queues_api, path=f"/{V1_ROOT}/{V1_QUEUES_ROUTE}")
+    api.add_namespace(mlflow_runs_api, path=f"/{V1_ROOT}/{V1_MLFLOW_RUNS_ROUTE}")
