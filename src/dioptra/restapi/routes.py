a00653e642004bd7088737ff0ecd4cb3a2243797
Keith Manville <kmanville@mitre.org>
2024-03-19 12:48:56 -0400

    feat: added jobs api to routes

--- src/dioptra/restapi/routes.py
+++ src/dioptra/restapi/routes.py
@@ -32,11 +32,12 @@ QUEUE_ROUTE = "queue"
 TASK_PLUGIN_ROUTE = "taskPlugin"
 USER_ROUTE = "user"
 
+V1_ARTIFACTS_ROUTE = "artifacts"
+V1_JOBS_ROUTE = "jobs"
+V1_MLFLOW_RUNS_ROUTE = "mlflow_runs"
 V1_PLUGINS_ROUTE = "plugins"
 V1_PLUGIN_PARAMETER_TYPES_ROUTE = "pluginParameterTypes"
 V1_QUEUES_ROUTE = "queues"
-V1_MLFLOW_RUNS_ROUTE = "mlflow_runs"
-V1_ARTIFACTS_ROUTE = "artifacts"
 
 
 def register_routes(api: Api) -> None:
@@ -77,18 +78,17 @@ def register_v1_routes(api: Api) -> None:
         api: The main REST |Api| object.
     """
     from .v1.artifacts.controller import api as artifacts_api
+    from .v1.jobs.controller import api as jobs_api
     from .v1.mlflow_runs.controller import api as mlflow_runs_api
     from .v1.plugin_parameter_types.controller import api as plugin_parameter_types_api
     from .v1.plugins.controller import api as plugins_api
     from .v1.queues.controller import api as queues_api
 
-    api.add_namespace(plugins_api, path=f"/{V1_ROOT}/{V1_PLUGINS_ROUTE}")
-    api.add_namespace(queues_api, path=f"/{V1_ROOT}/{V1_QUEUES_ROUTE}")
-    api.add_namespace(mlflow_runs_api, path=f"/{V1_ROOT}/{V1_MLFLOW_RUNS_ROUTE}")
     api.add_namespace(artifacts_api, path=f"/{V1_ROOT}/{V1_ARTIFACTS_ROUTE}")
+    api.add_namespace(jobs_api, path=f"/{V1_ROOT}/{V1_JOBS_ROUTE}")
+    api.add_namespace(mlflow_runs_api, path=f"/{V1_ROOT}/{V1_MLFLOW_RUNS_ROUTE}")
+    api.add_namespace(plugins_api, path=f"/{V1_ROOT}/{V1_PLUGINS_ROUTE}")
     api.add_namespace(
         plugin_parameter_types_api, path=f"/{V1_ROOT}/{V1_PLUGIN_PARAMETER_TYPES_ROUTE}"
     )
-    api.add_namespace(
-        plugin_parameter_types_api, path=f"/{V1_ROOT}/{V1_PLUGIN_PARAMETER_TYPES_ROUTE}"
-    )
+    api.add_namespace(queues_api, path=f"/{V1_ROOT}/{V1_QUEUES_ROUTE}")
