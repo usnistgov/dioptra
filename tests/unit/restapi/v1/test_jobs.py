f5b3b5fa84f68364b2524fd818b7d15cb2d4880e
Andrew Hand <ahand@mitre.org>
2024-04-09 15:27:36 -0400

    tests(restapi): Added tests for job status

--- tests/unit/restapi/v1/test_jobs.py
+++ tests/unit/restapi/v1/test_jobs.py
@@ -33,7 +33,7 @@ from werkzeug.test import TestResponse
 from dioptra.restapi.routes import EXPERIMENT_ROUTE, JOB_ROUTE, QUEUE_ROUTE, V1_ROOT
 from dioptra.restapi.v0.shared.s3.service import S3Service
 
-from .lib import mock_rq, mock_s3
+from dioptra.tests.unit.restapi.lib import mock_rq, mock_s3
 
 # -- Fixtures --------------------------------------------------------------------------
 
@@ -118,6 +118,7 @@ def submit_job(
         follow_redirects=True,
     )
 
+
 def delete_job(
         client: FlaskClient,
         id: int,
@@ -157,6 +158,24 @@ def assert_retrieving_job_by_id_works(
     """
     response = client.get(f"/{V1_ROOT}/{JOB_ROUTE}/{id}", follow_redirects=True)
     assert response.status_code == 200 and response.get_json() == expected
+
+
+def assert_retrieving_job_status_works(
+    client: FlaskClient, id: int, status: str
+) -> None:
+    """Assert that retrieving a job status works.
+
+    Args:
+        client: The Flask test client.
+        id: The id of the job to retrieve.
+        status: The expected status of the job.
+
+    Raises:
+        AssertionError: If the response status code is not 200 or if the API response
+            does not match the expected status.
+    """
+    response = client.get(f"/{V1_ROOT}/{JOB_ROUTE}/{id}/status", follow_redirects=True)
+    assert response.status_code == 200 and response.get_json() == status
 
 
 def assert_retrieving_all_jobs_works(
@@ -210,12 +229,19 @@ def test_submit_job(
             I need to submit a post request with that includes the experiment name, queue name, and job parameters
             In order to create a new job resource for the given experiment to be run on the given queue
 
+        Scenario: Get the Status of a Job Resource for an Experiment by its Unique ID
+            Given I am an authorized user and an experiment with jobs exists
+            I need to submit a get status request with a job id
+            In order to retrieve the status of a job resource matching the given id
+
     This test validates this scenario by following these actions:
 
     - A user submits a job.
     - The user retrieves information about the job that matches the information that
       was provided during registration id as an identifier.
     - The returned information matches what was provided during registration.
+    - The user retrieve information about the status of the posted job.
+    - The status matches the information recieved from the job submission.
     """
     # Inline import necessary to prevent circular import
     import dioptra.restapi.v0.shared.rq.service as rq_service
@@ -227,6 +253,7 @@ def test_submit_job(
     register_tensorflow_cpu_queue(client)
     job_expected = submit_job(client, form_request=job_request_factory()).get_json()  # noqa: B950; fmt: skip
     assert_retrieving_job_by_id_works(client, id=job_expected["jobId"], expected=job_expected)  # noqa: B950; fmt: skip
+    assert_retrieving_job_status_works(client, id=job_expected["jobId"], status=job_expected["status"]) # noqa: B950; fmt: skip
 
 
 @pytest.mark.v1
