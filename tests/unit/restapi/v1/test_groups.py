4b2f1126b3fbdf219315bc6b1f6fbc32494b1010
Andrew Hand <ahand@mitre.org>
2024-05-09 10:46:33 -0400

    build(restapi): Added unit tests for groups.

--- tests/unit/restapi/v1/test_groups.py
+++ tests/unit/restapi/v1/test_groups.py
@@ -87,6 +87,8 @@ def assert_group_response_contents_matches_expectations(
         "user",
         "createdOn",
         "lastModifiedOn",
+        "resources",
+        "tags",
     }
     assert set(response.keys()) == expected_keys
 
@@ -107,6 +109,19 @@ def assert_group_response_contents_matches_expectations(
     assert isinstance(response["user"]["username"], str)
     assert isinstance(response["user"]["url"], str)
     assert response["user"]["id"] == expected_contents["user_id"]
+
+    # TODO confirm that there are no more common feilds for various resourceRef types
+    # Validate the ResourceRef structure
+    for resource in response["resources"]:
+        assert isinstance(resource["id"], int)
+        assert isinstance(resource["name"], str)
+        assert isinstance(resource["url"], str)
+
+    # Validate the TagRef structure
+    for tag in response["tags"]:
+        assert isinstance(tag["id"], int)
+        assert isinstance(tag["name"], str)
+        assert isinstance(tag["url"], str)
 
 
 def assert_retrieving_group_by_id_works(
