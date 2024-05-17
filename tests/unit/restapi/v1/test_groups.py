6601fa75b1e6d0a3aeb600f36d83dbc968d269e5
andrewhand <95315138+andrewhand@users.noreply.github.com>
2024-05-17 11:17:38 -0400

    Update tests/unit/restapi/v1/test_groups.py
    Co-authored-by: James K. Glasbrenner <jglasbrenner@mitre.org>

--- tests/unit/restapi/v1/test_groups.py
+++ tests/unit/restapi/v1/test_groups.py
@@ -120,7 +120,7 @@ def assert_group_response_contents_matches_expectations(
         assert isinstance(member["read"], bool)
         assert isinstance(member["write"], bool)
         assert isinstance(member["share_read"], bool)
-        assert isinstance(member["share_right"], bool)
+        assert isinstance(member["share_write"], bool)
 
 
 def assert_retrieving_group_by_id_works(
