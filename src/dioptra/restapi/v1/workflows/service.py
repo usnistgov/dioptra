cf751164df4b11e04c5a7dce4e67eef6e387bd55
Andrew Hand <ahand@mitre.org>
2024-10-04 10:15:00 -0400

    build(restapi): add errors to workflow validation

--- src/dioptra/restapi/v1/workflows/service.py
+++ src/dioptra/restapi/v1/workflows/service.py
@@ -26,6 +26,7 @@ import yaml
 from .lib import views
 from .lib.package_job_files import package_job_files
 from .schema import FileTypes
+from .errors import EntrypointWorkflowValidationIssue
 
 from dioptra.restapi.db import db
 from dioptra.restapi.v1 import utils
@@ -134,7 +135,6 @@ class EntrypointValidateService(object):
         plugins = self._plugin_id_service.get(plugin_ids)
         for plugin in plugins:
             for plugin_file in plugin['plugin_files']:
-                # print(plugin_file.tasks)
                 for task in plugin_file.tasks:
                     input_parameters = task.input_parameters
                     output_parameters = task.output_parameters
@@ -160,16 +160,11 @@ class EntrypointValidateService(object):
             "tasks": tasks,
             "graph": cast(dict[str, Any], yaml.safe_load(task_graph)),
         }
-        print (task_engine_dict)
         valid = is_valid(task_engine_dict)
 
         if valid:
             return {"status": "Success", "valid": valid}
         else:
             issues = validate(task_engine_dict)
-            print(issues)
-            return {
-                "status": "Success", 
-                "valid": valid,
-                # "issues": issues,
-            }
+            log.debug("Entrypoint workflow validation failed", issues=issues)
+            raise EntrypointWorkflowValidationIssue(issues)
