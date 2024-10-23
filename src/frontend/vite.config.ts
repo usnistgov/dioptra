82484a0c098cd1f14bf0f9e7f46b9ab9c2e6dea0
henrychoy <hchoy@mitre.org>
2024-10-23 14:58:45 -0400

    fix: use absolute path for sass import

--- src/frontend/vite.config.ts
+++ src/frontend/vite.config.ts
@@ -3,6 +3,7 @@ import { fileURLToPath, URL } from 'node:url'
 import { defineConfig } from 'vite'
 import vue from '@vitejs/plugin-vue'
 import { quasar, transformAssetUrls } from '@quasar/vite-plugin'
+import path from 'path'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -13,7 +14,7 @@ export default defineConfig({
     // @quasar/plugin-vite options list:
     // https://github.com/quasarframework/quasar/blob/dev/vite-plugin/index.d.ts
     quasar({
-      sassVariables: 'src/quasar-variables.sass'
+      sassVariables: path.resolve(__dirname, 'src/quasar-variables.sass')
     })
   ],
   resolve: {
