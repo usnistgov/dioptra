838e2cc3c9eb0e28c973ba325fe6af057c2bbf3d
jtsextonMITRE <45762017+jtsextonMITRE@users.noreply.github.com>
2024-12-27 13:44:59 -0500

    examples: add client calls to plugins for artifacts and models

--- examples/task-plugins/dioptra_custom/fgm_mnist_demo/artifacts_restapi.py
+++ examples/task-plugins/dioptra_custom/fgm_mnist_demo/artifacts_restapi.py
@@ -14,157 +14,103 @@
 #
 # ACCESS THE FULL CC BY 4.0 LICENSE HERE:
 # https://creativecommons.org/licenses/by/4.0/legalcode
-import requests
 import structlog
 import os
 from dioptra import pyplugs
 from dioptra.client import connect_json_dioptra_client
 from structlog.stdlib import BoundLogger
-from posixpath import join as urljoin
-from urllib.parse import urlparse, urlunparse
 
 LOGGER: BoundLogger = structlog.stdlib.get_logger()
 
 @pyplugs.register    
 def get_uri_for_model(model_name, model_version=-1):
-    session, url = get_logged_in_session()
-    models = get(session, url, f'models?search={model_name}&pageLength=500')
+    dioptra_client = get_logged_in_session()
+    models = dioptra_client.models.get(
+        search=model_name, 
+        page_length=500
+    )
+    selected_model = None
     for model in models['data']:
         if (model['name'] == model_name):
             model_id = model['id']
             if (model_version >= 0):
-                selected_model = get(session, url, 
-                    f'models/{model_id}/versions/{model_version}'
+                selected_model = dioptra_client.models.versions.get_by_id(
+                    model_id=model_id, 
+                    version_number=model_version
                 )
             else:
                 selected_model = model['latestVersion']
 
-    uri = selected_model['artifact']['artifactUri']
+    uri = selected_model['artifact']['artifactUri'] if selected_model is not None else None
     return uri
 
 def get_uris_for_job(job_id):
-    session, url = get_logged_in_session()
-    job = get(session, url, 'jobs', str(job_id))
+    dioptra_client = get_logged_in_session()
+    job = dioptra_client.jobs.get_by_id(job_id)
     return [artifact['artifactUri'] for artifact in job['artifacts']]
 
 def get_uris_for_artifacts(artifact_ids):
-    session, url = get_logged_in_session()
-    return [get(session, url, 'artifacts', aid) for aid in artifact_ids]
+    dioptra_client = get_logged_in_session()
+    return [dioptra_client.artifacts.get_by_id(artifact_id=aid) for aid in artifact_ids]
 
 def get_logged_in_session():
-    session = requests.Session()
-    url = "http://dioptra-deployment-restapi:5000/api/v1"
-
-    login = post(session, url, {
-            'username':os.environ['DIOPTRA_WORKER_USERNAME'], 
-            'password':os.environ['DIOPTRA_WORKER_PASSWORD']},
-            'auth', 'login')
-    LOGGER.info("login request sent", response=str(login))
-
-    return session, url
+    url = "http://dioptra-deployment-restapi:5000/"
+    dioptra_client = connect_json_dioptra_client(url)
+    dioptra_client.auth.login(
+        username=os.environ['DIOPTRA_WORKER_USERNAME'], 
+        password=os.environ['DIOPTRA_WORKER_PASSWORD']
+    )
+    return dioptra_client
 
 def upload_model_to_restapi(name, source_uri, job_id):
     version = 0
     model_id = 0
     
-    session, url = get_logged_in_session()
+    dioptra_client = get_logged_in_session()
+    models = dioptra_client.models.get(
+        search=name,
+        page_length=500
+    )
 
-    models = get(session, url, f'models?search={name}&pageLength=500')
     LOGGER.info("requesting models from RESTAPI", response=models)
 
-    
     for model in models['data']:
         #check whether to create a new model
         if model['name'] == name:
             model_id = model['id']
             if model['latestVersion'] != None:
                 version = model['latestVersion']['versionNumber'] + 1
+    
     if (version == 0 and model_id == 0):
-        LOGGER.info("creating new model on RESTAPI")
-        model = post(session, url, {"group": 1, "name": name, "description": f"{name} model"}, "models")
+        model = dioptra_client.models.create(
+            group_id=1,
+            name=name,
+            description=f"{name} model"
+        )
         model_id = model['id']
         LOGGER.info("new model created", response=model)
     
-    artifact = post(session, url, {"group": 1, "description": f"{name} model artifact", "job": str(job_id), "uri": source_uri}, 'artifacts')
+    artifact = dioptra_client.artifacts.create(
+        group_id=1, 
+        description=f"{name} model artifact",
+        job_id=job_id, 
+        uri=source_uri
+    )
     LOGGER.info("artifact", response=artifact)
-    model_version = post(session, url, {"description": f"{name} model version", "artifact": artifact['id']}, 'models', str(model_id), 'versions')
+    
+    model_version = dioptra_client.models.versions.create(
+        model_id=model_id, 
+        artifact_id=artifact['id'], 
+        description=f"{name} model version"
+    )
     LOGGER.info("model created", response=model_version)
 
 def upload_artifact_to_restapi(source_uri, job_id):
-    session, url = get_logged_in_session()
-
-    artifact = post(session, url, {"group": 1, "description": f"artifact for job {job_id}", "job": str(job_id), "uri": source_uri}, 'artifacts')
+    dioptra_client = get_logged_in_session()
+    artifact = dioptra_client.artifacts.create(
+        group_id=1, 
+        description=f"artifact for job {job_id}", 
+        job_id=job_id,
+        uri=source_uri
+    )
     LOGGER.info("artifact", response=artifact)
-
-def debug_request(url, method, data=None):
-    LOGGER.debug("Request made.", url=url, method=method, data=data)
-
-
-def debug_response(json):
-    LOGGER.debug("Response received.", json=json)
-
-
-def get(session, endpoint, *features):
-    debug_request(urljoin(endpoint, *features), "GET")
-    return make_request(session, "get", endpoint, None, *features)
-
-
-def post(session, endpoint, data, *features):
-    debug_request(urljoin(endpoint, *features), "POST", data)
-    return make_request(session, "post", endpoint, data, *features)
-
-
-def delete(session, endpoint, data, *features):
-    debug_request(urljoin(endpoint, *features), "DELETE", data)
-    return make_request(session, "delete", endpoint, data, *features)
-
-
-def put(session, endpoint, data, *features):
-    debug_request(urljoin(endpoint, *features), "PUT", data)
-    return make_request(session, "put", endpoint, data, *features)
-
-
-def make_request(session, method_name, endpoint, data, *features):
-    url = urljoin(endpoint, *features)
-    method = getattr(session, method_name)
-    try:
-        if data:
-            response = method(url, json=data)
-        else:
-            response = method(url)
-        if response.status_code != 200:
-            raise StatusCodeError()
-        json = response.json()
-    except (requests.ConnectionError, StatusCodeError, requests.JSONDecodeError) as e:
-        handle_error(session, url, method_name.upper(), data, response, e)
-    debug_response(json=json)
-    return json
-
-
-def handle_error(session, url, method, data, response, error):
-    if type(error) is requests.ConnectionError:
-        restapi = os.environ["DIOPTRA_RESTAPI_URI"]
-        message = (
-            f"Could not connect to the REST API. Is the server running at {restapi}?"
-        )
-        LOGGER.error(message, url=url, method=method, data=data, response=response.text)
-        raise APIConnectionError(message)
-    if type(error) is StatusCodeError:
-        message = f"Error code {response.status_code} returned."
-        LOGGER.error(message, url=url, method=method, data=data, response=response.text)
-        raise StatusCodeError(message)
-    if type(error) is requests.JSONDecodeError:
-        message = "JSON response could not be decoded."
-        LOGGER.error(message, url=url, method=method, data=data, response=response.text)
-        raise JSONDecodeError(message)
-
-class APIConnectionError(Exception):
-    """Class for connection errors"""
-
-
-class StatusCodeError(Exception):
-    """Class for status code errors"""
-
-
-class JSONDecodeError(Exception):
-    """Class for JSON decode errors"""
