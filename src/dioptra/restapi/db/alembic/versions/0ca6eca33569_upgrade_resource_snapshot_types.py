60fe5494a1b691b2d35abfa74950d4206473a051
Cousin <dmitry.cousin@nist.gov>
2025-01-29 16:59:11 -0500

    fix: modify up/downgrade code to use only ORM alembic functionality

--- src/dioptra/restapi/db/alembic/versions/0ca6eca33569_upgrade_resource_snapshot_types.py
+++ src/dioptra/restapi/db/alembic/versions/0ca6eca33569_upgrade_resource_snapshot_types.py
@@ -6,12 +6,12 @@ Create Date: 2024-12-12 00:47:23.575103
 
 """
 
-from pprint import pprint
 from typing import Annotated, Any
 
 import sqlalchemy as sa
 from alembic import op
-from sqlalchemy import delete, insert, select
+
+# from sqlalchemy import delete, insert, select
 from sqlalchemy.engine.cursor import CursorResult
 from sqlalchemy.orm import (
     DeclarativeBase,
@@ -42,63 +42,44 @@ depends_on = None
 
 text_ = Annotated[str, mapped_column(sa.Text())]
 
+# The data to up/down-grade
+ENUM_TABLE = "resource_types"       ### The table to up/down-grade
+TABLE_ENTRY = 'resource_snapshot'   ### The entry to up/down-grade
 
-### DeclarativeBase does not allow direct subclassing !!!
-class AlmaTable(DeclarativeBase, MappedAsDataclass):
-    """The Mix-In Class of two base-classes that can not be directly inheritable
-    Args:
-        DeclarativeBase (_type_): Base Class for SqlAlchemy ORM
-        MappedAsDataclass (_type_): Base Class for SqlAlchemy ORM
-    """
 
+class UpgradeBase(DeclarativeBase, MappedAsDataclass):
     pass
 
+class DowngradeBase(DeclarativeBase, MappedAsDataclass):
+    pass
 
-class ResourceTypes(AlmaTable):
-    """The extension class for Table ORM Model in SqlAlchemy::ORM module
-    Args:
-        AlmaTable (_type_): The Mix-In Class of two SqlAlchemy ORM base-classes that can't be directly inheritable
-    Returns:
-        _type_: ResourceTypes ORM model of the resource_types table in the underlying DB
-    """
-
-    __tablename__ = "resource_types"
-    resource_type: Mapped[text_] = mapped_column(nullable=False, primary_key=True)
-    # -------------------------------------------------------------------------
-
-    def __repr__(self):
-        return f"{self.resource_type}"
-
-    # -------------------------------------------------------------------------
+class ResourceTypeUpgrade(UpgradeBase):
+    __tablename__ = ENUM_TABLE
+    resource_type: Mapped[text_] = mapped_column(primary_key=True)
 
 
-def execute_in_session(statement) -> CursorResult | None:
-    """Reusable function to Execute SQL-Alchemy statement in a session
-    Args:
-        statement (_type_): _description_
-    """
-    result: CursorResult | None = None
-    bind = op.get_bind()
-    Session = sessionmaker(bind=bind)
-    with Session() as session:
-        result = session.execute(statement)
-        session.commit()
-    # pprint(f'{dir(result)=}')
-    return result
-    # -----------------------------------------------------------------------------
+class ResourceTypeDowngrade(DowngradeBase):
+    __tablename__ = ENUM_TABLE
+    resource_type: Mapped[text_] = mapped_column(primary_key=True)
 
 
 def upgrade():
     """Alembic Upgrade Hook-Point that deletes the entry from
     resource_types table with value 'resource_snapshot'
     """
-    delete_statement = (
-        # Upgrade means death.
-        # Death of the 'resource_snapshot'
-        # resource_type row entry
-        delete(ResourceTypes).where(ResourceTypes.resource_type == "resource_snapshot")
-    )
-    execute_in_session(delete_statement)
+    bind = op.get_bind()
+    Session = sessionmaker(bind=bind)
+
+    with Session() as session:
+        stmt = sa.select(ResourceTypeUpgrade).where(
+            ResourceTypeUpgrade.resource_type == TABLE_ENTRY
+        )
+        resource_snapshot_type = session.scalar(stmt)
+
+        if resource_snapshot_type is not None:
+            session.delete(resource_snapshot_type)
+
+        session.commit()
     # -----------------------------------------------------------------------------
 
 
@@ -106,27 +87,17 @@ def downgrade():
     """Alembic Downgrade Hook-Point that reinstates the entry to
     resource_types table with value 'resource_snapshot'
     """
-    ### Generic SQL INSERT (!!! particularly psql dialect !!!) doesn't have
-    ### .on_conflict_do_nothing(index_elements=['resource_type'])
-    ### Fixing 'Insert' object has no attribute 'on_conflict_do_nothing'
-    select_statement = select(ResourceTypes).where(
-        ResourceTypes.resource_type == "resource_snapshot"
-    )
-    result = execute_in_session(select_statement)
-    ### Brutal, forceful, but more efficient that highly discouraged func.count in query
-    ### Unfortunately it is required to assure we don't end up with table
-    ### containing multiple resource_snapshot rows
-    count = 0
-    if result:
-        for row in result:
-            count += 1
-    # Done counting: if not 0 ignore
-    if not result or count == 0:
-        insert_statement = (
-            # Downgrade means rebirth.
-            # Rebirth of the 'resource_snapshot'
-            # resource_type row entry
-            insert(ResourceTypes).values(resource_type="resource_snapshot")
+    bind = op.get_bind()
+    Session = sessionmaker(bind=bind)
+
+    with Session() as session:
+        stmt = sa.select(ResourceTypeUpgrade).where(
+            ResourceTypeUpgrade.resource_type == TABLE_ENTRY
         )
-        execute_in_session(insert_statement)
+        resource_snapshot_type = session.scalar(stmt)
+
+        if resource_snapshot_type is None:
+            session.add(ResourceTypeUpgrade(resource_type=TABLE_ENTRY))
+
+        session.commit()
     # -----------------------------------------------------------------------------
