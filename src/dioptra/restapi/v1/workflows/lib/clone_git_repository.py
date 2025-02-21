495373a9e6b334325a8ab7f1238f10a472a7fe52
Keith Manville <kmanville@mitre.org>
2025-02-21 09:25:08 -0500

    fix: added check for presense of git command

--- src/dioptra/restapi/v1/workflows/lib/clone_git_repository.py
+++ src/dioptra/restapi/v1/workflows/lib/clone_git_repository.py
@@ -1,3 +1,4 @@
+import shutil
 import subprocess
 from pathlib import Path
 from urllib.parse import urlparse
@@ -27,10 +28,14 @@ def clone_git_repository(url: str, dir: Path) -> str:
     git_paths = parsed_url.params or None
     git_url = parsed_url._replace(fragment="", params="").geturl()
 
+    cmd = shutil.which("git")
+    if cmd is None:
+        raise RuntimeError("Git command not found.")
+
     git_env = {"GIT_TERMINAL_PROMPT": "0"}
     git_sparse_args = ["--filter=blob:none", "--no-checkout", "--depth=1"]
     git_branch_args = ["-b", git_branch] if git_branch else []
-    clone_cmd = ["git", "clone", *git_sparse_args, *git_branch_args, git_url, str(dir)]
+    clone_cmd = [cmd, "clone", *git_sparse_args, *git_branch_args, git_url, str(dir)]
     clone_result = subprocess.run(
         clone_cmd, env=git_env, capture_output=True, text=True
     )
@@ -42,7 +47,7 @@ def clone_git_repository(url: str, dir: Path) -> str:
 
     if git_paths is not None:
         paths = git_paths.split(",")
-        sparse_checkout_cmd = ["git", "sparse-checkout", "set", "--cone", *paths]
+        sparse_checkout_cmd = [cmd, "sparse-checkout", "set", "--cone", *paths]
         sparse_checkout_result = subprocess.run(
             sparse_checkout_cmd, cwd=dir, capture_output=True, text=True
         )
@@ -52,7 +57,7 @@ def clone_git_repository(url: str, dir: Path) -> str:
                 sparse_checkout_result.returncode, sparse_checkout_result.stderr
             )
 
-    checkout_cmd = ["git", "checkout"]
+    checkout_cmd = [cmd, "checkout"]
     checkout_result = subprocess.run(
         checkout_cmd, cwd=dir, capture_output=True, text=True
     )
@@ -62,7 +67,7 @@ def clone_git_repository(url: str, dir: Path) -> str:
             checkout_result.returncode, checkout_result.stderr
         )
 
-    hash_cmd = ["git", "rev-parse", "HEAD"]
+    hash_cmd = [cmd, "rev-parse", "HEAD"]
     hash_result = subprocess.run(hash_cmd, cwd=dir, capture_output=True, text=True)
 
     if hash_result.returncode != 0:
