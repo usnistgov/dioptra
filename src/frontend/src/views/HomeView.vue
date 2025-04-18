992889375c135c21bfbec2e018c8b1f4c28b7ada
Keith Manville <kmanville@mitre.org>
2025-04-18 09:54:57 -0400

    feat(frontend): add component to import resources
    This feature adds a resource import dialog that is accessible via a new icon in the nav bar. The
    dialog supports importing resources from a git repository, archive file, or directory. It uses the
    existing resource import workflow in the REST API.

--- src/frontend/src/views/HomeView.vue
+++ src/frontend/src/views/HomeView.vue
@@ -6,15 +6,27 @@
     learning experiments.
   </p>
   <p>
+    To get started, consider importing the example entrypoints and plugins distributed with Dioptra <a href="#" @click="showImportDialog = true">here</a>.
+  </p>
+  <p>
     For reference and instructions, you can view our docs here: <br>
     <a href="https://pages.nist.gov/dioptra/" target="_blank">
       https://pages.nist.gov/dioptra/
     </a>
   </p>
 
-
+  <ImportResourcesDialog v-model="showImportDialog" />
 </template>
 
 <script setup>
+  import { useQuasar } from 'quasar'
+  import { computed } from 'vue'
+  const $q = useQuasar()
 
+  import ImportResourcesDialog from '../dialogs/ImportResourcesDialog.vue';
+  const showImportDialog = defineModel()
+
+  const darkMode = computed(() => {
+    return $q.dark.mode
+  })
 </script>
