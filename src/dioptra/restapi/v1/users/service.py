a4c3521e0907d79441cf12bbbd93dc7a3fab4ebe
Cousin <dmitry.cousin@nist.gov>
2025-07-30 21:06:52 -0400

    feat(i561) add enumerated entity types for better code quality
    - added enum
    - went over the Dioptra error interfaces to modify common exceptions and errors
    - cleaned the imports and started enum unit-test
    - resolved merge conflicts

--- src/dioptra/restapi/v1/users/service.py
+++ src/dioptra/restapi/v1/users/service.py
@@ -21,6 +21,7 @@ import uuid
 from typing import Any, Final, cast
 
 import structlog
+from entity_types import EntityTypes
 from flask_login import current_user
 from injector import inject
 from structlog.stdlib import BoundLogger
